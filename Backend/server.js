b98506b90fff30620da5ef02080817055c89e4d5
Laura Polet <laurapplt@gmail.com>
2024-09-18 14:30:13 +0200

    Install Express

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,14 @@
 const http = require ('http');
-const server = http.createServer((req, res) => {
-    res.end('voilà la réponse du premier serveur');
-}); 
 
+const app = require('./App');
+
+const server = http.createServer(app);
+
+// const server = http.createServer((req, res) => { res.end('voilà la réponse du premier serveur');}); 
 //serveur prêt mais doit attendre les requetes envoyées, pour ceci on utilise la methode listen() du serveur. 
+
+// dire à l'application sur quel port elle va tourner
+app.set('port', process.env.PORT || 3000)
 
 
 server.listen(process.env.PORT || 3000);
