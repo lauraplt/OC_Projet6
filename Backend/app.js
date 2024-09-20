12fd435aa410b6e5449b6cbfacf78fe2fbd154a9
Laura Polet <laurapplt@gmail.com>
2024-09-20 10:30:48 +0200

    server.js improvement

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,12 @@ const express = require ('express');
 
 const app = express ();
 
-app.use((req, res) => {
+app.use((req,res, next) => {
+    console.log('Requête reçue !');
+    next(); //pour passer sur le second middleware, car sinon on ne renvoie pas de réponse et la requête ne se termine pas
+});
+
+app.use((req, res, next) => {
     res.json({message: 'Votre requête a bien été reçue'});
     });
 
