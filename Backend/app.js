efe2b352ccd67b8e4843759e0aba7ff85b550a24
Laura Polet <laurapplt@gmail.com>
2024-09-24 12:10:39 +0200

    installation mongoose

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,22 @@
 const express = require ('express');
-
+const mongoose =require('mongoose');
 const app = express ();
+app.use(express.json());
 
-app.use((req,res, next) => {
-    console.log('Requête reçue !');
-    next(); //pour passer sur le second middleware, car sinon on ne renvoie pas de réponse et la requête ne se termine pas
-});
-
+//CORS
 app.use((req, res, next) => {
-    res.json({message: 'Votre requête a bien été reçue'});
-    });
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  mongoose.connect('mongodb+srv://lauraplt:hello2024@cluster0.cqmwn.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-module.exports = app;
 
+    module.exports = app;
 
+    //lauraplt:<db_password>@cluster0.cqmwn.mongodb.net/?
