bdc650801e62df06f7c3ea3361c8ded99458cad3
Laura Polet <laurapplt@gmail.com>
2024-09-25 16:15:09 +0200

    add PUT (modify) and DELETE

--- Backend/controller/Books.js
+++ Backend/controller/Books.js
@@ -1,4 +1,4 @@
-const book = require('../models/book');
+const Book = require('../models/book');
 
 //POST to register a book 
 exports.createBook = (req, res, next) => {
@@ -23,5 +23,59 @@ exports.createBook = (req, res, next) => {
 exports.getOneBook = (req, res, next) => {
     Book.findOne({ _id: req.params.id })
         .then(book => res.status(200).json(book))
+        .catch(error => res.status(404).json({ error }));
+};
+
+//PUT
+exports.modifyBook = (req, res, next) => {
+    const bookObject = req.file ? {
+        ...JSON.parse(req.body.book),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/resized_${req.file.filename}`
+    } : { ...req.body };
+    delete bookObject._userId;
+    book.findOne({_id: req.params.id})
+    .then((book) => {
+        if(book.userId != req.auth.userId) {
+            res.status(403).json({ message : '403: unauthorized request' });
+        }else{
+            const FILE_NAME = book.imageUrl.split('/images/')[1];
+            req.file && FileSystem.unlink('images/${FILE_NAME}', (err => {
+                    if (err) console.log(err);
+                })
+            );
+        
+            book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+            .catch(error => res.status(400).json({ error }));
+        }
+    })
+    .catch((error) => {
+        res.status(404).json({ error });
+    });
+};
+
+//DELETE
+exports.deleteBook = (req, res, next) => {
+    book.findOne({ _id: req.params.id })
+    .then(book => {
+        if (book.userId != req.auth.userId) {
+            res.status(403).json({ message: '403: unauthorized request'});
+        } else {
+            const FILE_NAME = book.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${FILE_NAME}`, () => {
+                book.deleteOne({ _id: req.params.id })
+                .then(() => { res.status(200).json({ message: 'Objet Suprimé !'}) })
+                .catch(error => res.status(400).json({ error }));
+            })
+        }
+    })
+    .catch( error => {
+        res.status(404).json({ error });
+    });
+};
+//GET
+exports.getAllBooks = (req, res, next) => {
+    book.find()
+        .then(books => res.status(200).json(books))
         .catch(error => res.status(404).json({ error }));
 };
